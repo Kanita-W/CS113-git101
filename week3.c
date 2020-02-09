c165226794bf51004702906bfad20bcc2a10552a
Kanita Wansang <kanita.w@ku.th>
2020-02-09 20:14:53 +0700

    short note week3

---
+++ week3.c
@@ -0,0 +1,48 @@
+// 1. Statements and Blocks //
+
+// ต้องปิดท้ายด้วย ; เช่น x = 0 --> x = 0 ; 
+// เครื่องหมายปีกกา {} หลัง for, while, eles if, main
+
+// 2. If-Else 
+// เรามักจะเขียนอยู่ในรูป if (เงื่อนไข) 
+
+// 3. Else-if
+//  ex. if ()
+        // statement1
+    //  else if ()
+        // statement2 
+
+ // 4. Switch
+ // จะทำงานไปเรื่อยๆจนกว่าจะเจอ break แล้วจึงจะหลุดจาก switch
+
+        // รูปแบของ swich case
+// switch () {
+   /* case :             // << case เหมือน if  
+       statements
+    case :
+       statements
+    default:            // << default เหมือน else
+       statements /*
+//}
+
+/* 5. Loops-While and For
+        // รูปแบบของ while
+        while ()
+           statements
+           
+           รูปแบบของ For
+        for (เงื่อนไขกำหนดครั้งแรก; เงื่อนไขให้ทำต่อจนครบ; เงื่อนไข+ วนรอบ){} /*
+
+// 6. Loop-Do-While
+        // รูแปบบของ Do-while
+        do {
+            statement
+        } while () ;
+    // state จะถูกทำงานซึ่งเงื่อนไขใน while จะดูว่า state เป็นจริงมั้ยถ้าเป็นจริงทำวนไปเรื่อยๆ แต่ถ้าเป็นเท็จให้ทำก่อน 1 ครั้งแล้วจึงหยุด
+
+
+// 7. Break and Continue
+    // break และ continue จะช่วยคนเขียนให้สามารถควบคุมการวนซ้ำได้มากขึ้น
+
+    break จะทำให้โปรแกรมหลุดออกมาจากลูป for, while และ do-while โดยทันที ทั้งนี้การหลุดจากลูปจะทำจากลูปวงในสุดที่ break อยู่
+
